622bbc16d3abd9a4898cefb4e9cadbc36150544d
misohan <michal.hanusniak@aiesec.cz>
2020-09-06 13:06:58 +0200

    Add check for multiplying by number 5

--- src/main/java/com/codecool/FizzBuzz.java
+++ src/main/java/com/codecool/FizzBuzz.java
@@ -3,7 +3,9 @@ package com.codecool;
 public class FizzBuzz {
 
     public String convert(int i) {
-
+        if ( i% 5==0){
+            return "Buzz";
+        }
         if (i % 3 == 0){
             return "Fizz";
         }
